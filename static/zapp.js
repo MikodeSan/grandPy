71d52df6e4e1e6cee5378d53b5a59cd5935bcc33
tousmi01 <mickael.toussaint@somfy.com>
2019-12-17 10:51:32 +0100

    User query displayed into the dialog field
    Submit by pressing 'Enter' key or click on image
    No web page refresh needed

--- static/zapp.js
+++ static/zapp.js
@@ -1,23 +1,22 @@
 const dialog = document.getElementById('dialog');
 console.log(dialog);
 
-const nDialog = 10;
 
-const addReply = (_strReply, _idx) => {
-    
+const addReply = (_strReply) => {
+
     div = addTextDiv();
     time = localTime();
 
-    div.innerHTML = "<p>" + time + " - " + _strReply + " #" + _idx + "</p>";
+    div.innerHTML = "<p>" + time + " - " + _strReply + "</p>";
     div.classList.add("reply_style");
 }
 
-const addQuery = (_strUserQuery, _idx) => {
-    
+const addQuery = (_strUserQuery) => {
+
     div = addTextDiv();
     time = localTime();
 
-    div.innerHTML = "<p>" + _strUserQuery + " #" + _idx + " - " + time + "</p>";
+    div.innerHTML = "<p>" + _strUserQuery + " - " + time + "</p>";
     div.classList.add("query_style");
 }
 
@@ -35,13 +34,22 @@ const localTime = () => {
 }
 
 
-for (let i = 0; i < nDialog; i++) {
-    addQuery("my new query", i);
-    addReply("my new reply", i);
-}
+const elt = document.getElementById('user_input_form');
+elt.addEventListener('submit', function(event) {
+
+    event.preventDefault();
+    event.stopPropagation();
+
+    const userText_form = document.getElementById('query_text');
+
+    strUserText = userText_form.value;
+    if (strUserText) {
+
+        addQuery(strUserText);
+        addReply(strUserText);
+    }
+    userText_form.value = "";
+});
 
 //elt.removeChild(newElt);    // Supprime l'élément newElt de l'élément elt
 //elt.replaceChild(document.createElement("article"), newElt);    // Remplace l'élément newElt par un nouvel élément de type article
-
-
-
