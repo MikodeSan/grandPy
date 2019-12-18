36e940d7ef49af6bfb9f7019f58201b85b43ced3
tousmi01 <mickael.toussaint@somfy.com>
2019-12-18 11:04:27 +0100

    Web page blocks Refactoring
    delete non applicable files
    set dynamic url
    get paremeters

--- z_app/views.py
+++ z_app/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
@@ -21,18 +21,10 @@ def index():
 @app.route('/zapp/')
 @app.route('/zapp.html/')
 def zapp():
-    return render_template('zapp.html')
 
-
-# @app.route('/result/')
-# def result():
-#     description = """
-#         Toi, tu n'as pas peur d'être seul ! Les grands espaces et les aventures sont faits pour toi. D'ailleurs, Koh Lanta est ton émission préférée ! Bientôt tu partiras les cheveux au vent sur ton radeau. Tu es aussi un idéaliste chevronné. Quelle chance !
-#     """
-#     return render_template('result.html',
-#                             user_name='Tom',
-#                             user_image=url_for('static', filename='tmp/cover_111823112767411.jpg'),
-#                             description=description)
+    user_name = request.args.get('user_name')
+    return render_template('zapp.html',
+                            user_name=user_name)
 
 
 if __name__ == "__main__":
